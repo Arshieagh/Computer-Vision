2906d19ca66ce86fa6910da85f0120f7460e78c8
Arshieagh <arshieagh@gmail.com>
2025-12-18 23:55:27 +0000

    Add residual connections

--- models/baseline.py
+++ models/baseline.py
@@ -2,27 +2,62 @@ import torch
 from torch import nn
 
 
+class ResidualBlock(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.conv1 = nn.Conv2d(channels, channels, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(channels)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.conv2 = nn.Conv2d(channels, channels, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(channels)
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
 class BaseLineModel(nn.Module):
     input_channels = 3
     num_classes = 100
 
     def __init__(self):
         super(BaseLineModel, self).__init__()
-        self.features = nn.Sequential(
-            # Layer 1
+
+        self.stem = nn.Sequential(
             nn.Conv2d(self.input_channels, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(),
+        )
+
+        self.block1 = nn.Sequential(
+            ResidualBlock(64),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            # Layer 2
+        )
+
+        self.block2 = nn.Sequential(
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU(),
+            ResidualBlock(128),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            # Layer 3
+        )
+        self.block3 = nn.Sequential(
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
             nn.BatchNorm2d(256),
             nn.ReLU(),
+            ResidualBlock(256),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
 
@@ -36,7 +71,10 @@ class BaseLineModel(nn.Module):
         )
 
     def forward(self, x):
-        x = self.features(x)
+        x = self.stem(x)
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
         x = torch.flatten(x, start_dim=1)
         x = self.classifier(x)
         return x
