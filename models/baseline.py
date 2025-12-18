1c316aa69eaba589a307dd49e6a23393cb2a5d30
Arshieagh <arshieagh@gmail.com>
2025-12-18 23:14:08 +0000

    Add batch norm to the baseline model

--- models/baseline.py
+++ models/baseline.py
@@ -9,13 +9,19 @@ class BaseLineModel(nn.Module):
     def __init__(self):
         super(BaseLineModel, self).__init__()
         self.features = nn.Sequential(
+            # Layer 1
             nn.Conv2d(self.input_channels, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
+            # Layer 2
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
+            # Layer 3
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
@@ -23,6 +29,7 @@ class BaseLineModel(nn.Module):
         # Classifier
         self.classifier = nn.Sequential(
             nn.Linear(256 * 4 * 4, 512),
+            nn.BatchNorm1d(512),
             nn.ReLU(),
             nn.Dropout(0.5),
             nn.Linear(512, self.num_classes),
