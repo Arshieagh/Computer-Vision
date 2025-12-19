da7966f4bdd325393c23da60c62e7800585d7915
Arshieagh <arshieagh@gmail.com>
2025-12-19 00:46:09 +0000

    Fixed hierarchical multi-task learning (fine + coarse heads)

--- data/cifar100.py
+++ data/cifar100.py
@@ -1,6 +1,124 @@
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
+import torch
+
+# CIFAR-100 fine-to-coarse mapping
+CIFAR100_FINE_TO_COARSE = [
+    4,
+    1,
+    14,
+    8,
+    0,
+    6,
+    7,
+    7,
+    18,
+    3,
+    3,
+    14,
+    9,
+    18,
+    7,
+    11,
+    3,
+    9,
+    7,
+    11,
+    6,
+    11,
+    5,
+    10,
+    7,
+    6,
+    13,
+    15,
+    3,
+    15,
+    0,
+    11,
+    1,
+    10,
+    12,
+    14,
+    16,
+    9,
+    11,
+    5,
+    5,
+    19,
+    8,
+    8,
+    15,
+    13,
+    14,
+    17,
+    18,
+    10,
+    16,
+    4,
+    17,
+    4,
+    2,
+    0,
+    17,
+    4,
+    18,
+    17,
+    10,
+    3,
+    2,
+    12,
+    12,
+    16,
+    12,
+    1,
+    9,
+    19,
+    2,
+    10,
+    0,
+    1,
+    16,
+    12,
+    9,
+    13,
+    15,
+    13,
+    16,
+    19,
+    2,
+    4,
+    6,
+    19,
+    5,
+    5,
+    8,
+    19,
+    18,
+    1,
+    2,
+    15,
+    6,
+    0,
+    17,
+    8,
+    14,
+    13,
+]
+
+
+class CIFAR100WithCoarse(torch.utils.data.Dataset):
+    def __init__(self, cifar100_dataset):
+        self.dataset = cifar100_dataset
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        image, fine_label = self.dataset[idx]
+        coarse_label = CIFAR100_FINE_TO_COARSE[fine_label]
+        return image, (fine_label, coarse_label)
 
 
 # Data loading
@@ -20,23 +138,27 @@ def load_data(batch_size=64):
         ]
     )
 
-    train_dataset = datasets.CIFAR100(
-        root="./data",
-        train=True,
-        transform=train_transform,
-        download=True,
-        target_type=["fine", "coarse"],
+    train_dataset = CIFAR100WithCoarse(
+        datasets.CIFAR100(
+            root="./data",
+            train=True,
+            transform=train_transform,
+            download=True,
+            target_type=["fine", "coarse"],
+        )
     )
     train_dataloader = DataLoader(
         train_dataset, batch_size=batch_size, shuffle=True, num_workers=2
     )
 
-    test_dataset = datasets.CIFAR100(
-        root="./data",
-        train=False,
-        transform=test_transform,
-        download=True,
-        target_type=["fine", "coarse"],
+    test_dataset = CIFAR100WithCoarse(
+        datasets.CIFAR100(
+            root="./data",
+            train=False,
+            transform=test_transform,
+            download=True,
+            target_type=["fine", "coarse"],
+        )
     )
     test_dataloader = DataLoader(
         test_dataset, batch_size=batch_size, shuffle=False, num_workers=2
